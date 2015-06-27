98bec35c88004ff5173cf87c247337f2604e1d66
mtdukes <tyler.dukes@gmail.com>
2015-06-26 21:57:13 -0400

    added code road map

---
+++ clervoyance.py
@@ -0,0 +1,17 @@
+#Load the existing csv data, if it exists
+
+#If the data doesn't exist, create a new csv
+
+#Read in the DOE Clery act page
+
+#Load schools listed in the <select> tag with id="schoolDD"
+
+#If the school doesn't already exist in the database, navigate to the page and download all new reports
+
+#If the school exists in the database, navigate to the page and check if the reports listed match those in the database
+
+#If the report does not exist in the database, download it
+
+#Upload all new reports to the S3 bucket
+
+#Update JSON file in S3 bucket describing reports in the database (JS/footables in the html will render as sortable table on page load)
